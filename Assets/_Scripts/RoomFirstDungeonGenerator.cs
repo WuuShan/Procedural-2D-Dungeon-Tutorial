6d30c59a3b12206475b02eb5469fed54dfdcf640
WuuShan <805490972@qq.com>
2022-12-01 22:30:12 +0800

    p16 使用走廊连接房间

--- Assets/_Scripts/RoomFirstDungeonGenerator.cs
+++ Assets/_Scripts/RoomFirstDungeonGenerator.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 /// <summary>
 /// 房间优先地牢生成器
@@ -47,10 +48,103 @@ public class RoomFirstDungeonGenerator : SimpleRandomWalkDungeonGenerator
             new Vector3Int(dungeonWidth, dungeonHeight, 0)), minRoomWidth, minRoomHeight);
 
         HashSet<Vector2Int> floor = new HashSet<Vector2Int>();
+
         floor = CreateSimpleRooms(roomsList);
+
+        List<Vector2Int> roomCenters = new List<Vector2Int>();
+        foreach (var room in roomsList)
+        {
+            roomCenters.Add((Vector2Int)Vector3Int.RoundToInt(room.center));
+        }
+
+        HashSet<Vector2Int> corridors = ConnectRooms(roomCenters);
+        floor.UnionWith(corridors);
 
         tilemapVisualizer.PaintFloorTiles(floor);
         WallGenerator.CreateWalls(floor, tilemapVisualizer);
+    }
+
+    /// <summary>
+    /// 根据房间中心点列表用走廊连接各个房间
+    /// </summary>
+    /// <param name="roomCenters">房间中心点列表</param>
+    /// <returns>走廊位置</returns>
+    private HashSet<Vector2Int> ConnectRooms(List<Vector2Int> roomCenters)
+    {
+        HashSet<Vector2Int> corridors = new HashSet<Vector2Int>();
+        var currentRoomCenter = roomCenters[Random.Range(0, roomCenters.Count)];    // 当前房间中心点
+        roomCenters.Remove(currentRoomCenter);
+
+        while (roomCenters.Count > 0)
+        {
+            Vector2Int closest = FindClosestPointTo(currentRoomCenter, roomCenters);
+            roomCenters.Remove(closest);
+            HashSet<Vector2Int> newCorridor = CreateCorridor(currentRoomCenter, closest);
+            currentRoomCenter = closest;
+            corridors.UnionWith(newCorridor);
+        }
+        return corridors;
+    }
+
+    /// <summary>
+    /// 在当前房间中心点和目标房间中心点之间创建走廊
+    /// </summary>
+    /// <param name="currentRoomCenter">当前房间中心点</param>
+    /// <param name="destination">目标房间中心点</param>
+    /// <returns>走廊位置</returns>
+    private HashSet<Vector2Int> CreateCorridor(Vector2Int currentRoomCenter, Vector2Int destination)
+    {
+        HashSet<Vector2Int> corridor = new HashSet<Vector2Int>();
+        var position = currentRoomCenter;
+        corridor.Add(position);
+        while (position.y != destination.y)
+        {
+            if (destination.y > position.y)
+            {
+                position += Vector2Int.up;
+            }
+            else if (destination.y < position.y)
+            {
+                position += Vector2Int.down;
+            }
+            corridor.Add(position);
+        }
+        while (position.x != destination.x)
+        {
+            if (destination.x > position.x)
+            {
+                position += Vector2Int.right;
+            }
+            else if (destination.x < position.x)
+            {
+                position += Vector2Int.left;
+            }
+            corridor.Add(position);
+        }
+        return corridor;
+    }
+
+    /// <summary>
+    /// 查找距离当前房间中心点最近的房间中心点
+    /// </summary>
+    /// <param name="currentRoomCenter">当前房间中心点</param>
+    /// <param name="roomCenters">房间中心点列表</param>
+    /// <returns>最近房间中心点</returns>
+    private Vector2Int FindClosestPointTo(Vector2Int currentRoomCenter, List<Vector2Int> roomCenters)
+    {
+        Vector2Int closest = Vector2Int.zero;   // 最近房间中心点
+        float distance = float.MaxValue;        // 最近房间中心点距离
+        foreach (var position in roomCenters)
+        {
+            float currentDistance = Vector2.Distance(position, currentRoomCenter);  // 当前房间中心点距离
+            // 如果 当前房间中心点距离 小于 最近房间中心点距离
+            if (currentDistance < distance)
+            {
+                distance = currentDistance;
+                closest = position;
+            }
+        }
+        return closest;
     }
 
     /// <summary>
