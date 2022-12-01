6165bb79d22da65e45675774c1b4898d1a04bb88
WuuShan <805490972@qq.com>
2022-12-01 14:01:47 +0800

    p13

--- Assets/_Scripts/ProceduralGenerationAlgorithms.cs
+++ Assets/_Scripts/ProceduralGenerationAlgorithms.cs
@@ -51,68 +51,6 @@ public static class ProceduralGenerationAlgorithms
         }
         return corridor;
     }
-
-    /// <summary>
-    /// 二叉空间划分
-    /// </summary>
-    /// <param name="spaceToSplit">要划分的空间</param>
-    /// <param name="minWidth">最小宽度</param>
-    /// <param name="minHeight">最小高度</param>
-    /// <returns>房间列表</returns>
-    public static List<BoundsInt> BinarySpacePartitioning(BoundsInt spaceToSplit, int minWidth, int minHeight)
-    {
-        Queue<BoundsInt> roomsQueue = new Queue<BoundsInt>();   // 房间队列
-        List<BoundsInt> roomsList = new List<BoundsInt>();      // 房间列表
-        roomsQueue.Enqueue(spaceToSplit);   // 将要划分的空间入队
-        while (roomsQueue.Count > 0)    // 判断队里还有没有人
-        {
-            var room = roomsQueue.Dequeue();    // 拿到第一个队员
-            if (room.size.y >= minHeight && room.size.x >= minWidth)    // 判断房间的大小是否大于等于最小空间
-            {
-                if (Random.value < 0.5f)
-                {
-                    if (room.size.y >= minHeight * 2)   // 房间高度大于两倍的最小高度
-                    {
-                        SplitHorizontally(minWidth, minHeight, roomsQueue, room);
-                    }
-                    else if (room.size.x >= minWidth * 2)   // 房间宽度大于两倍的最小宽度
-                    {
-                        SplitVertically(minWidth, minHeight, roomsQueue, room);
-                    }
-                    else
-                    {
-                        roomsList.Add(room);
-                    }
-                }
-                else
-                {
-                    if (room.size.x >= minWidth * 2)
-                    {
-                        SplitVertically(minWidth, minHeight, roomsQueue, room);
-                    }
-                    else if (room.size.y >= minHeight * 2)
-                    {
-                        SplitHorizontally(minWidth, minHeight, roomsQueue, room);
-                    }
-                    else
-                    {
-                        roomsList.Add(room);
-                    }
-                }
-            }
-        }
-        return roomsList;
-    }
-
-    private static void SplitVertically(int minWidth, int minHeight, Queue<BoundsInt> roomsQueue, BoundsInt room)
-    {
-        throw new System.NotImplementedException();
-    }
-
-    private static void SplitHorizontally(int minWidth, int minHeight, Queue<BoundsInt> roomsQueue, BoundsInt room)
-    {
-        throw new System.NotImplementedException();
-    }
 }
 
 /// <summary>
